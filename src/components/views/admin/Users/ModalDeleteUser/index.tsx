f890c5dfc16131de05c2e5f6645d50a99ecfb647
rizqullahanjr <rizqullahanjr@gmail.com>
2024-11-03 12:25:14 +0700

    git ignore case false update

--- src/components/views/admin/Users/ModalDeleteUser/index.tsx
+++ src/components/views/admin/Users/ModalDeleteUser/index.tsx
@@ -5,24 +5,26 @@ import userServices from "@/services/user";
 import styles from "./ModalDeleteUser.module.scss";
 
 const ModalDeleteUser = (props: any) => {
-    const { deletedUser, setDeletedUser, setUsersData } = props;
-    const handleDelete = async () => {
-        userServices.deleteUser(deletedUser.id);
-        setDeletedUser({});
-        const { data } = await userServices.getAllUsers();
-        setUsersData(data.data);
-    }
-    return (
-        <Modal onClose={() => setDeletedUser({})}>
-            <h1 className={styles.modal__title}>Are you sure?</h1>
-            <Button 
-                type="button" 
-                onClick={() => {handleDelete()}}
-                >
-                    Delete
-                </Button>
-        </Modal>
-    )
+  const { deletedUser, setDeletedUser, setUsersData } = props;
+  const handleDelete = async () => {
+    userServices.deleteUser(deletedUser.id);
+    setDeletedUser({});
+    const { data } = await userServices.getAllUsers();
+    setUsersData(data.data);
+  };
+  return (
+    <Modal onClose={() => setDeletedUser({})}>
+      <h1 className={styles.modal__title}>Are you sure?</h1>
+      <Button
+        type="button"
+        onClick={() => {
+          handleDelete();
+        }}
+      >
+        Delete
+      </Button>
+    </Modal>
+  );
 };
 
 export default ModalDeleteUser;

5f10dd26c73565a1ebf8d3388f886f00bd6429bc
rizqullahanjr <rizqullahanjr@gmail.com>
2024-11-06 01:17:34 +0700

    feat: Auth API

--- src/components/views/admin/Users/ModalUpdateUser/index.tsx
+++ src/components/views/admin/Users/ModalUpdateUser/index.tsx
@@ -5,10 +5,12 @@ import Input from "@/components/ui/Input";
 import Modal from "@/components/ui/Modal";
 import Select from "@/components/ui/Select";
 import userServices from "@/services/user";
+import { useSession } from "next-auth/react";
 import { FormEvent, useState } from "react";
 
 const ModalUpdateUser = (props: any) => {
   const { updatedUser, setUpdatedUser, setUsersData } = props;
+  const session: any = useSession();
   const [isLoading, setIsLoading] = useState(false);
   const handleUpdateUser = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -18,7 +20,11 @@ const ModalUpdateUser = (props: any) => {
       role: form.role.value,
     };
 
-    const result = await userServices.updateUser(updatedUser.id, data);
+    const result = await userServices.updateUser(
+      updatedUser.id,
+      data,
+      session.data?.accessToken
+    );
     if (result.status === 200) {
       setIsLoading(false);
       setUpdatedUser({});
